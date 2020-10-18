0387ad88d4e5ffb54b6e7e27a0b8981cf9cd31c7
stevenclarke94 <stevenjsclarke@yahoo.co.uk>
2020-10-18 15:06:56 +0100

    Updates

---
+++ precache-manifest.78a85ae60fad081b5f8b47ba79d66e30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cb90eb2cce9e863bd1aa88558ace43",
+    "url": "/steventodo/index.html"
+  },
+  {
+    "revision": "d3e7899731a1b3b2ddc9",
+    "url": "/steventodo/static/css/main.d93b8de9.chunk.css"
+  },
+  {
+    "revision": "d5b1756076b25da5e9a6",
+    "url": "/steventodo/static/js/2.45ca4794.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/steventodo/static/js/2.45ca4794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e7899731a1b3b2ddc9",
+    "url": "/steventodo/static/js/main.1c2a188f.chunk.js"
+  },
+  {
+    "revision": "c738e819d665e72416d3",
+    "url": "/steventodo/static/js/runtime-main.92f0d867.js"
+  }
+]);
